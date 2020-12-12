8c788ebb791882b67111ceff05dcd167ebf84856
Chance Lane <lanecw@gmail.com>
2020-12-12 11:18:53 -0500

    variable practice

---
+++ script.js
@@ -0,0 +1,48 @@
+let firstName = 'Chance'
+let lastName = 'Lane'
+let age = 42
+let likesTravel = true
+
+
+Chance = {
+    firstName:'Chance',
+    lastName:'Lane',
+    age:42,
+    likesToTravel:true,
+
+}
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(likesTravel)
+
+movieArray = ['Frosty', 'Rudolph', 'grinch']
+console.log(movieArray)
+
+console.log(movieArray[0])
+console.log(movieArray[1])
+console.log(movieArray[2])
+
+for (i=0; i<movieArray.length; i++){
+    console.log(movieArray[i])
+}
+
+let frosty = {
+    title: 'Frosty',
+    director: 'Herbert Hoover',
+    year: 1993,
+    }
+
+console.log(frosty)
+console.log(frosty.year)
+
+frosty.director = 'chucks';
+console.log(frosty.director)
+frosty.cast = ['chucks', 'chjkhda', 'fdsafjdsafsdaf']
+
+let x = 'fdafds'
+
+Chance.firstName = x
+this is a gitbash test
+test2
